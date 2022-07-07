df1f66be627fd84bdffe2451eaec42d0ca66c841
Willian Santana <willianleonardorv@gmail.com>
2022-07-07 13:06:15 -0300

    Subindo primeira versão

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+
+app.use(cors());
+
+app.get("/", (req, res)=>{
+    res.json({message:"SUCESSO!"});
+});
+
+app.get("/teste", (req, res)=>{
+    res.json({message:"TESTE SUCESSO!"});
+});
+
+app.listen(3000,()=>{
+    console.log("Servidor startado com sucesso!")
+});
