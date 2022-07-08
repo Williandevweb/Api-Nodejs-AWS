5e1e4f09ea749d4c96db9b9dedd769c0c0457015
Willian - Desenvolvedor <80816594+Williandevweb@users.noreply.github.com>
2022-07-07 23:47:13 -0300

    Update index.js

--- index.js
+++ index.js
@@ -12,6 +12,4 @@ app.get("/teste", (req, res)=>{
     res.json({message:"TESTE SUCESSO!"});
 });
 
-app.listen(3000, () => {
-    Console.log("Servidor rodando na porta 3000");
-});
+app.listen(3000);
