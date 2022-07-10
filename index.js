49d7acb6ea06684e3fe35183152e509969f66e84
Willian - Desenvolvedor <80816594+Williandevweb@users.noreply.github.com>
2022-07-10 11:21:40 -0300

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.use(express.json())
 app.use(cors());
 
 app.get("/", (req, res)=>{
-    res.send(JSON.stringify(req.params));
+    //res.send(JSON.stringify(req.params));
 });
 
 //app.get("/", (req, res)=>{
