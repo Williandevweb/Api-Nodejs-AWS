29508c081a0a62207d2d9fa67267d1bf35ab5033
Willian - Desenvolvedor <80816594+Williandevweb@users.noreply.github.com>
2022-07-10 18:02:44 -0300

    Update index.js

--- index.js
+++ index.js
@@ -7,12 +7,12 @@ app.use(express.json())
 app.use(cors());
 
 app.get("/", (req, res)=>{
-    res.send(JSON.stringify(req.params));
-});
-
-app.get("/", (req, res)=>{
   res.sendFile(__dirname + '/index.html')
 })
+
+app.get("/tray/callback/auth", (req, res)=>{
+    res.send(JSON.stringify(req.params));
+});
 
 app.listen(3000,()=>{
     console.log("api run")
